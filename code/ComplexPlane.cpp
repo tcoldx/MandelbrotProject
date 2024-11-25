e4a46773b2e3f15bd3f4ffe20193a5fa2ae47a06
💰JayAre💰 Rico <jmail0627@gmail.com>
2024-11-24 22:03:58 -0800

    added draw

--- code/ComplexPlane.cpp
+++ code/ComplexPlane.cpp
@@ -8,11 +8,15 @@ using namespace std;
 
 //constructor
 ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
-    : m_pixel_size({ pixelWidth, pixelHeight }),
+    : m_pixel_size({pixelWidth, pixelHeight}),
       m_vArray(Points, pixelWidth* pixelHeight),
       m_zoomCount(0),
       m_plane_center({ 0, 0 }),
-      m_plane_size({ BASE_WIDTH, BASE_HEIGHT * static_cast<float>(pixelHeight) / pixelWidth }),
+      m_plane_size({BASE_WIDTH, BASE_HEIGHT * static_cast<float>(pixelHeight) / pixelWidth}),
       m_state(State::CALCULATING) {
     m_aspectRatio = static_cast<float>(pixelHeight) / pixelWidth;
+}
+
+void ComplexPlane::draw(RenderTarget& target, RenderStates states) const {
+    target.draw(m_vArray);
 }
