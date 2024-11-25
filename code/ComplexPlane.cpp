41ea1616da0864eaed4dd736ad5438bca0845ac1
💰JayAre💰 Rico <jmail0627@gmail.com>
2024-11-24 22:00:21 -0800

    added constructor

--- code/ComplexPlane.cpp
+++ code/ComplexPlane.cpp
@@ -1,8 +1,18 @@
-#include <iostream>
-#include <SFML/Graphics.hpp> 
-#include <vector>
 #include "ComplexPlane.h"
+#include <cmath>
+#include <iostream>
+#include <sstream>
 
 using namespace sf;
 using namespace std;
 
+//constructor
+ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
+    : m_pixel_size({ pixelWidth, pixelHeight }),
+      m_vArray(Points, pixelWidth* pixelHeight),
+      m_zoomCount(0),
+      m_plane_center({ 0, 0 }),
+      m_plane_size({ BASE_WIDTH, BASE_HEIGHT * static_cast<float>(pixelHeight) / pixelWidth }),
+      m_state(State::CALCULATING) {
+    m_aspectRatio = static_cast<float>(pixelHeight) / pixelWidth;
+}
